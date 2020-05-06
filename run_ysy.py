7f99a561e26e621353d48d8697fa09c3adc8aa03
JaneYSY <janeysy0504@gmail.com>
2020-05-06 15:59:41 -0400

    Update run_ysy.py

--- run_ysy.py
+++ run_ysy.py
@@ -4,84 +4,97 @@ from PyQt5.QtWidgets import *
 import sys
 import random
 import math
+from 
 
-
+class Cruise(metaclass=Singleton):
+    def __init__(self, cruise_centerx, cruise_centery):
+        self.cruise_centerx = cruise_centerx
+        self.cruise_centery = cruise_centery
 
 
 
 class Bed():
-	def __init__(self,)
+    def __init__(self, bed_number):
+        self.bed_number = bed_number
+
+    def bed_change(self, bed_number):
+        '''
+        This is to add bed if bed is not enough. Bed is supposed to be added meet isolation need.
+
+        **parameter**
+
+        **output**
+        '''
+        current_bed_number = 20
+        if isolated > current_bed_number:
+            current_bed_number += sick - current_bed_number
+        return current_bed_number
 
 
 class Singleton(object):
-	_instance = None
-	
-	def __new__(cls, *args, **kwargs):
-		if cls._instance is None:
-			cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
-		return cls._instance
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
 
 
 # Parameters
 class Parameters():
-	
-	#total population on the cruise
-	total_population = 2500
 
-	current_time = 1 # by day
+    # total population on the cruise
+    total_population = 2500
 
-	# size of cruise, for plotting
-	cruise_width = 500
-	cruise_length = 2000
-	cruise_centerx = 250
-	cruise_centery = 1000
+    current_time = 1  # by day
 
-	# Number of people who are infected (brought the disease on board)
-	patients_zero = random.randint(1, 10)
+    # size of cruise, for plotting
+    cruise_width = 500
+    cruise_length = 2000
+    cruise_centerx = 250
+    cruise_centery = 1000
 
-	# rate of death for infected patients
-	fatal_rate = 0.12
+    # Number of people who are infected (brought the disease on board)
+    patients_zero = random.randint(1, 10)
 
-	#days take to die
-	death_period = 15
+    # rate of death for infected patients
+    fatal_rate = 0.12
 
-	#variance of days to die
-	#以后看看要不要写
+    # days take to die
+    death_period = 15
 
-	# number of people a sick individual can infect
-	reproduction_rate = 2
+    # variance of days to die
+    # 以后看看要不要写
 
-	#virus latency period
-	latency_period = random.randint(2, 14)
+    # number of people a sick individual can infect
+    reproduction_rate = 2
 
-	#response latency - delay of a sick person getting treatment, such as isolation
-	response_latency = 2
+    # virus latency period
+    latency_period = random.randint(2, 14)
 
-	#safe distance of social distancing
-	safe_distance = 2
+    # response latency - delay of a sick person getting treatment, such as isolation
+    response_latency = 2
 
-	#moving factor - how much movement is allowed, [0, 5]
-	moving_factors = 2
+    # safe distance of social distancing
+    safe_distance = 2
+
+    # moving factor - how much movement is allowed, [0, 5]
+    moving_factors = 2
+    # variance of moving
+    moving_sigma = 3
 
 
 class Constant():
-	healthy = 0
-	susceptible = healthy + 1
-	latency = susceptible + 1
-	sick = latency + 1
-	isolated = sick + 1 # isolated people, location frozen
-	death = isolated + 1 # dead people, location frozen, cannot transmit
+    healthy = 0
+    susceptible = 1
+    latency = 2
+    sick = 3
+    isolated = 4  # isolated people, location frozen
+    death = 5  # dead people, location frozen, cannot transmit
 
 
 # 单件模式
-class 
-
-
-
-
-
-
-
+class
 
 
 if __name__ == "__main__":
@@ -92,6 +105,5 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
 
     MainWindow.show()
-    
-    sys.exit(app.exec_())
 
+    sys.exit(app.exec_())
