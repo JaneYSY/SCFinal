45658e7f492f180aef1e417ab0cf3d27e74555f0
Alfie Chen <baomeisha@icloud.com>
2020-05-08 13:35:02 -0400

    Small adjustment.

--- run_ysy.py
+++ run_ysy.py
@@ -6,53 +6,27 @@ import math
 import numpy as np
 
 
-# class Singleton(type):
-#     """
-#     For some classes, only ONE instance is allowed. Singleton is to define here using metaclass,
-#     and metaclass to define the behavior of a class and its instance.
-#     """
+class Point:
+    def __init__(self, loc_x, loc_y):
+        self.x = loc_x
+        self.y = loc_y
 
-#     _instances = {}
+    def move(self, vec_x, vec_y):
+        self.x += vec_x
+        self.y += vec_y
 
-#     def __call__(cls, *args, **kwargs):
-#         if cls not in cls._instances:
-#             cls._instances[cls] = super(
-#                 Singleton, cls).__call__(*args, **kwargs)
-#         return cls._instances[cls]
-
-    """ Title: Python Course: Metaclasses - Creating Singletons using Metaclasses
-    Author: Bernd Klein
-    URL: https://www.python-course.eu/python3_metaclasses.php
-    We decide to put the Singleton into each targeted class. """
-
-
-'''
-class Cruise:
-    _instance = None
-    def __new__(cls, *args, **kw):
-        if cls._instance is None:
-            cls._instance = object.__new__(cls, *args, **kw)
-        return cls._instance
-    def __init__(self, cruise_centerx, cruise_centery):
-        self.centerx = cruise_centerx
-        self.centery = cruise_centery
-'''
+    def move_to(self, loc_x, loc_y):
+        self.x = loc_x
+        self.y = loc_y
 
 
 class Bed:
-    """
-    Number of bed.
-    Bed and IsoRoom are separated is because IsoRoom is Singleton for metaclass.
-    """
-
     def __init__(self, bed_number):
         self.bed_number = bed_number
         self.occupied = False
 
 
 class IsoRoom:
-
-    # Singleton
     _instance = None
 
     def __new__(cls, *args, **kw):
@@ -64,7 +38,6 @@ class IsoRoom:
         self.occupied_beds = 0
         self.available_beds = Parameters.iso_room_capacity
         self.need_beds = 0
-        # A list of bed to assign individual bed's condition.
         self.beds_list = []
         for i in range(0, Parameters.iso_room_capacity):
             self.beds_list.append(Bed(i))
@@ -88,36 +61,8 @@ class IsoRoom:
         return bed
 
 
-class Point:
-    """
-    This is to describe invididual as points.
-    Points move with velocity.
-    Points move from and to location which are spots (x, y) on the plot. 
-    """
-
-    def __init__(self, loc_x, loc_y):
-        self.x = loc_x
-        self.y = loc_y
-
-    def move(self, vec_x, vec_y):
-        self.x += vec_x
-        self.y += vec_y
-
-    def move_to(self, loc_x, loc_y):
-        self.x = loc_x
-        self.y = loc_y
-
-
 class Person(Point):
-<<<<<<< HEAD
     def __init__(self, loc_x, loc_y):
-=======
-    """
-    Person inherits from Point class. It describes each person's condition [individual condition].
-    """
-
-    def __init__(self, loc_x, loc_y, cruise):
->>>>>>> f5679331896ae67feaf8d1ca4898859d70ea7846
         super(Person, self).__init__(loc_x, loc_y)
         self.status = Condition.healthy
         self.infected_time = 0
@@ -129,14 +74,13 @@ class Person(Point):
 
 
 class Parameters:
-    """
-    Parameters to be called in refreshing everyday's change. [Overall condition]
-    """
     game_over = False
-    total_population = 1000  # total population on the cruise
+    # total population on the cruise
+    total_population = 1000
+
     current_day = 1
 
-    # Size of cruise, for plotting
+    # size of cruise, for plotting
     cruise_width = 1000
     cruise_hight = 1000
     cruise_centerx = 500
@@ -145,42 +89,40 @@ class Parameters:
     # Number of people who are infected (brought the disease on board)
     patients_zero = 5
 
-    fatal_rate = 0.08  # rate of death for infected patients
-    trans_prob = 0.8  # if you are in contact with an infected person, the chance you'll get sick
+    # rate of death for infected patients
+    fatal_rate = 0.08
+    trans_prob = 0.8
 
-    death_period = 15  # days take to die
-    death_period_var = 15  # variance of days to die
+    # days take to die
+    death_period = 15
 
-    incubation_period = 7  # virus incubation period
+    # variance of days to die
+    death_period_var = 15
 
-    iso_latency = 2  # response latency - delay of a sick person getting isolation
+    # virus incubation period
+    incubation_period = 7
+
+    # response latency - delay of a sick person getting isolation
+    iso_latency = 2
+
     iso_room_capacity = 100
 
     # safe distance of virus
     safe_distance = 2
 
-    flow_intention = 3  # motivation for an individual to move on the cruise
+    flow_intention = 3
 
     # normal_sigma = 1
     # normal_t_sigma = 50
 
 
 class Condition:
-    """
-    Condition of each person.
-    """
-
     healthy = 0
     susceptible = 1
-<<<<<<< HEAD
     incubation = 2
-=======
-    latency = 2  # incubation period
->>>>>>> f5679331896ae67feaf8d1ca4898859d70ea7846
     sick = 3
     isolated = 4  # isolated people, location frozen
     death = 5  # dead people, location frozen, cannot transmit
-
 
 
 class Track:
@@ -217,14 +159,15 @@ class Pool:
             loc_y = random.uniform(0, Parameters.cruise_hight)
             self.all.append(Person(loc_x, loc_y))
 
-    def count_condition(self, condition_code=None):
-        if condition_code == None:
+    def count_status(self, condition_code=None):
+        if condition_code is None:
             return len(self.all)
         count = 0
         for person in self.all:
             if person.status == condition_code:
                 count += 1
         return count
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
